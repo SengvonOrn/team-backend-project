b7718009ee04bd35e110184d3d2953e285177a15
seng <sengvonkh@gmail.com>
2025-10-18 20:04:58 +0700

    add module and docker  file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, UsersModule, ProductsModule],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
